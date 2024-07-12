9e3b0c964f5649fd35d66ab4502d34f1b446fe35
Breno Henrique Nascimento <brenonsc@gmail.com>
2024-07-12 08:53:10 -0300

    refactor(libcsv): restructure project, organize files

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #!/bin/bash
 
-# Compila libcsv.c
-gcc -c libcsv.c -o libcsv.o
+# Compila main.c, adicionando o diretório libcsv ao caminho de pesquisa de cabeçalhos
+gcc -c main.c -o main.o -Ilibcsv
 
-# Compila main.c
-gcc -c main.c -o main.o
+# Compila libcsv.c
+gcc -c libcsv/libcsv.c -o libcsv.o
 
 # Linka os objetos e cria o executável
 gcc libcsv.o main.o -o app
